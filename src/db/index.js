7c6a26c1b08fb618dd004dd80b3cf903aa59baaa
Shreyak Timsina <shreyaktimsina29@gmail.com>
2024-03-19 13:06:59 +0545

    connectionInstance issue resolved

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,9 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n MongoDB Connected! DB HOST: ${connectionInstance}`);
+    console.log(
+      `\n MongoDB Connected! DB HOST: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("MongoDB Connection Error", error);
     process.exit(1);
